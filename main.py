41daf8c75468362dc91a2722307408b4f8ef9ce1
Rebanto Nath <rebanto.nath@gmail.com>
2025-12-11 22:14:44 -0500

    add space to plot

--- main.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-from sklearn.datasets import make_moons # simple datatset for training the model
---
+++ start.py
@@ -0,0 +1,5 @@
+import numpy as np
+from sklearn.datasets import make_moons # simple datatset for training the model
+import matplotlib.pyplot as plt
+
+# plot the dataset below!
